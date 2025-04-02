29b0841ec23936e4d7b07d55e19a472a986a8637
rse0013 <sssrse.s0001@gmail.com>
2025-04-02 18:20:53 +0300

    Update SHABLON01_ino.ino

--- SHABLON01_ino/SHABLON01_ino.ino
+++ SHABLON01_ino/SHABLON01_ino.ino
@@ -6,17 +6,24 @@
 //long - для всего
 //------setup - Название
 //------() - круглые скобки (SHIFT+9, SHIFT+0) настройки
-//------{} - фигурные скобки (SHIFT+Х, SHIFT+Ъ) Начало и конец)
+//------{} - фигурные скобки (SHIFT+Х, SHIFT+Ъ) Начало и конец
+//------[] - квадратные скобки 
 //Сложных приказов
 const byte dynPin = 2;
 //-------------------
-#define BUTTON_PIN 7
 int lastButtonState; [2](https://arduinogetstarted.com/faq/arduino-how-to-detect-button-press-event)
 //----------------------------------------
 //
+
+// мини игра нажми на кнопку - угадай последовательность
+// загараются пиксели разного цвета, нужно нажать на кнопку такого цвета.
+
 //способности:
 //включить пиксель на матрице
-void pix() {
+
+//a номер
+//b номер
+void pix(int a, int b) {
   
 }
 //проиграть цепочку цветов
@@ -24,18 +31,31 @@ void cvet(){
 
 }
 
+int k[5]={0,0,0,0,0}
+
 //проверить кнопку
-void knop() {
+void knop(int a) {
  // читаем значение кнопки
-    int buttonState = digitalRead(BUTTON_PIN);
+    int buttonState = digitalRead(a+1);
     if (lastButtonState != buttonState) {  // состояние изменилось
         delay(50);  // время антидребезга
         if (buttonState == LOW)
-            Serial.println("Нажатие кнопки");
+            Serial.println("Нажатие кнопки ");
+             Serial.print(a);
+            k[a]=1;
         else
-            Serial.println("Отпускание кнопки");
+            Serial.println("Отпускание кнопки ");
+             Serial.print(a);
+
+            
         lastButtonState = buttonState;
     }
+}
+
+void reset(){
+  for (int a=1; a<#k; a++){
+    k[a]=0;
+  }
 }
 
 //издать звук
@@ -63,13 +83,77 @@ Serial.begin(9600);
 
 void loop() {
 //триггеры
-
+  if (s==1){
+    for (int a=1; a<#k; a++){
+       if (k[a]==1){
+        s=2;
+        reset();
+        break
+       }
+     }
+  }
 //------------
 //состояния
   //ждать
   if (s==1){
     
   }
+//
+//работа
+
+//1=красный
+//2=зелёный
+//3=синий
+//4=белый
+//5=ораневый
+
+  if (s==2){
+    //задать последовательнось на уровень 
+    int cep[5]={1,2,3,4,5}
+    //засветить последовательность
+    for (int a=1; a<#cep; a++){
+      pix(math.random(1,64),cep[a])
+    }
+    //захватить последовательность нажатий
+    // int otv={}
+      int otv={}
+    // while (#otv<#cep){
+      while (#onv<#cep){
+    //   for (int a=1; a<#k; a++){
+         for (int a=1; a<#)
+    //    if (k[a]==1){
+
+    //     otv[a]=a
+
+    //     reset();
+
+    //     break
+          break
+    //    }
+        }
+    //  }
+       }
+    // }
+      }
+    //сравнить результат изменить состояние на выиграл или проиграл
+      
+  }
+//
+// выиграл
+  if (s==3){
+    
+  }
+//
+// проиграл
+  if (s==4){
+    
+  }
+//
+// разряжен
+  if (s==5){
+    
+  }
+//
 //------------
 
 }
